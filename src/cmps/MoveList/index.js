42e82e915920cbd60d9357efe671fc815f2e58d6
lizamir <lizamir2@gmail.com>
2021-06-23 15:00:48 +0300

    first commit

--- src/cmps/MoveList/index.js
+++
@@ -1,1 +0,0 @@
-export { MoveList } from './MoveList'
--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import '../src/assets/scss/main.scss'
-import { store } from './store';
-import { Provider } from 'react-redux';
+// import { store } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <App />
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
